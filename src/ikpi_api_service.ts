3f359f41389a694768b4e7dfe319d90525ae082f
ElRaptorus <christian.werner@5minds.de>
2018-08-16 16:52:17 +0200

    Make kpi service methods async

--- src/ikpi_api_service.ts
+++ src/ikpi_api_service.ts
@@ -6,31 +6,39 @@ import {AverageFlowNodeRuntime} from './average_flownode_runtime';
 export interface IKpiApiService {
   /**
    * Gets the AverageFlowNodeRuntime for every FlowNode in a give ProcessModel.
+   *
+   * @async
    * @param processModelId The Id of the PorcessModel.
    * @param identity The identity to check claims for.
    */
-  getAverageRuntimeForProcessModel(identity: IIdentity, processModelId: string): Array<AverageFlowNodeRuntime>;
+  getAverageRuntimeForProcessModel(identity: IIdentity, processModelId: string): Promise<Array<AverageFlowNodeRuntime>>;
 
   /**
    * Gets the AverageFlowNodeRuntime for a specific FlowNode inside a ProcessModel.
+   *
+   * @async
    * @param processModelId The Id of the ProcessModel.
    * @param flowNodeId The Id of the specific FlowNode from whcih to get the average runtime.
    * @param identity The identity to check claims for.
    */
-  getAverageRuntimeForFlowNode(identity: IIdentity, processModelId: string, flowNodeId: string): AverageFlowNodeRuntime;
+  getAverageRuntimeForFlowNode(identity: IIdentity, processModelId: string, flowNodeId: string): Promise<AverageFlowNodeRuntime>;
 
   /**
    * Gets all active Tokens for a given ProcessModelId.
+   *
+   * @async
    * @param processModelId The Id of the ProcessModel.
    * @param identity The identity to check claims for.
    */
-  getActiveTokensForProcessModel(identity: IIdentity, processModelId: string): Array<ActiveToken>;
+  getActiveTokensForProcessModel(identity: IIdentity, processModelId: string): Promise<Array<ActiveToken>>;
 
   /**
    * Gets all active Tokens for a specific FlowNode inside a ProcessModel.
+   *
+   * @async
    * @param processModelId The Id of the ProcessModel.
    * @param flowNodeId The Id of the sepcific FlowNode from whcih to get the active Tokens.
    * @param identity The identity to check claims for.
    */
-  getActiveTokensForFlowNode(identity: IIdentity, flowNodeId: string): Array<ActiveToken>;
+  getActiveTokensForFlowNode(identity: IIdentity, flowNodeId: string): Promise<Array<ActiveToken>>;
 }
