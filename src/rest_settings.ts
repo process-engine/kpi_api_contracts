98461a8dc515a46a8eef25917f41cfabd9a50908
Steffen <Steffen.knaup@5minds.de>
2019-01-02 16:01:34 +0100

    :sparkles: Add getActiveTokensForProcessInstance

--- src/rest_settings.ts
+++ src/rest_settings.ts
@@ -1,6 +1,7 @@
 // tslint:disable:typedef
 const params = {
   correlationId: 'correlation_id',
+  processInstanceId: ':process_instance_id',
   processModelId: ':process_model_id',
   flowNodeId: ':flow_node_id',
 };
@@ -9,6 +10,7 @@ const paths = {
   getRuntimeInformationForProcessModel: `/process_model/${params.processModelId}/runtime_information`,
   getActiveTokensForProcessModel: `/process_model/${params.processModelId}/active_tokens`,
   getActiveTokensForCorrelationAndProcessModel: `/correlation/${params.correlationId}/process_model/${params.processModelId}/active_tokens`,
+  getActiveTokensForProcessInstance: `/process_instance/${params.processInstanceId}/active_tokens`,
   getRuntimeInformationForFlowNode: `/process_model/${params.processModelId}/flow_node/${params.flowNodeId}/runtime_information`,
   getActiveTokensForFlowNode: `/flow_node/${params.flowNodeId}/active_tokens`,
 };
